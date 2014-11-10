b7b075d02163469c8eee5cfdd489765092c907a3
Ira <ira@irahochman.com>
2014-11-10 10:48:18 -0500

    implements complete musician type facet and EMI calcuation in AnnInc Output

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -14,30 +14,91 @@ class DashboardController < ApplicationController
     #
     # Musician Type facet
     #
-    @role = (params.has_key?(:role)) ? params[:role] : "ALL"
-    case @role
-    when "COMPOSER"
-      @sample = @sample.where(role_composer: true)
-    when "RECORDING"
-      @sample = @sample.where(role_recording: true)      
-    when "SALARIED"
-      @sample = @sample.where(role_salaried: true)
-    when "PERFORMER"
-      @sample = @sample.where(role_performer: true)
-    when "SESSION"
-      @sample = @sample.where(role_session: true)
+    facetroles=0
+    roleclause=""
+    if ("true"==params[:role_missing]) then 
+         roleclause = "(role_composer is null AND role_recording is null AND role_salaried is null AND role_performer is null AND role_session is null)"
+         facetroles+=1
+    end
+    if ( "true"==params[:role_composer] ) then 
+      if 0!=facetroles
+        roleclause += " OR role_composer=true"
+      else
+        roleclause = "role_composer=true"
+      end
+      facetroles+=1
+      #@role_composer=true
+    end
+    if ( "true"==params[:role_recording] ) then 
+       if 0!=facetroles
+         roleclause = roleclause + " OR role_recording=true" 
+       else
+         roleclause = "role_recording=true"
+       end
+       facetroles+=1
+      #@role_recording=true
+    end
+    if ( "true"==params[:role_salaried] ) then 
+       if 0!=facetroles
+         roleclause = roleclause + " OR role_salaried=true" 
+       else
+         roleclause = "role_salaried=true"
+       end
+       facetroles+=1
+      #@role_salaried=true
+    end
+    if ( "true"==params[:role_performer] ) then 
+       if 0!=facetroles
+         roleclause = roleclause + " OR role_performer=true" 
+       else
+         roleclause = "role_performer=true"
+       end
+       facetroles+=1
+      #@role_performer=true
+    end
+    if ("true"==params[:role_session]) then 
+       if 0!=facetroles
+         roleclause = roleclause + " OR role_session=true" 
+       else
+         roleclause = "role_session=true"
+       end
+       facetroles+=1
+      #@role_session=true
+    end
+    if 0<facetroles then
+        @sample = @sample.where(roleclause)
+    end
+    
+ 
+#    when "COMPOSER"
+#      @sample = @sample.where(role_composer: true)
+#    when "RECORDING"
+#      @sample = @sample.where(role_recording: true)      
+#    when "SALARIED"
+#      @sample = @sample.where(role_salaried: true)
+#    when "PERFORMER"
+#      @sample = @sample.where(role_performer: true)
+#    when "SESSION"
+#      @sample = @sample.where(role_session: true)
     # else
       # else stmts
-    end
+#    end
     #
     # Career Level facet
     #
     @careerexp = params.has_key?(:careerexp) ? params[:careerexp] : "ALL"
     #logger.info{"careerexp #{params.has_key?(:careerexp)} and is #{@careerexp}"}
     
+    #
+    # AnnInc 
+    # TODO Refactor into AnnInc method & Model
+    #
+    @AvgEMI = @sample.average(:EMI)
+    @EMISampleSize = @sample.count(:EMI)
+    @AvgEMI ||= 0 # if nil (or false) set to 0
     
     #
-    #  TODO Refactor into FTClaim method
+    #  TODO Refactor into FTClaim method & Model
     #
     @sample_size = @sample.count()
     numerator = @sample.where(:full_time => true).count()
