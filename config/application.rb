a451d3953074ce7698257b6fc5b20e69bc13ce27
Brian O'Neill <info@rhythmspice.com>
2015-10-23 21:28:04 -0400

    Removing COMPASS; creating my own mixins for now.

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,7 @@ Bundler.require(*Rails.groups)
 module Ptest
   class Application < Rails::Application
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-      config.assets.paths.concat(
-      Compass::Frameworks::ALL.map { |f| f.stylesheets_directory }
-    )
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
